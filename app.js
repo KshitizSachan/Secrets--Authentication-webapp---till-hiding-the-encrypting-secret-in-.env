9cf65c7eb28542eb495bd753f448636a22c4938b
KshitizSachan <kshitizsachan422@gmail.com>
2022-12-30 01:40:51 +0530

    3rd commit

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt= require("mongoose-encryption");
-
+// const encrypt= require("mongoose-encryption");
+const md5=require("md5");
 
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://kshitiz:9450603282@cluster0.cacxhu4.mongodb.net/userDB");
@@ -15,7 +15,7 @@ const userSchema=new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User=new mongoose.model("User", userSchema);
 
@@ -48,7 +48,7 @@ app.post("/register", function(req,res){
     // Creating a new user
     const newUser=new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err)
@@ -60,7 +60,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if(err)
